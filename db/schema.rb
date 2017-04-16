97300019d1a9506b1f3593e4b90ec43027f7ba59
Jackson Graziano <jackgraziano@gmail.com>
2017-04-16 07:38:24 -0300

    Color model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415212220) do
+ActiveRecord::Schema.define(version: 20170416103804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170415212220) do
   create_table "clients", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "colors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
